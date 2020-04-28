8878c32d828b8e602cf72a4dd000d1d7b6901deb
dlengel <dlengel@debian.home.com>
2020-04-28 18:28:50 -0400

    Add new classes, remove paddles from main.

---
+++ matchmanager.cpp
@@ -0,0 +1,6 @@
+#include "matchmanager.h"
+
+MatchManager::MatchManager()
+{
+
+}
---
+++ matchmanager.h
@@ -0,0 +1,11 @@
+#ifndef MATCHMANAGER_H
+#define MATCHMANAGER_H
+
+
+class MatchManager
+{
+public:
+    MatchManager();
+};
+
+#endif // MATCHMANAGER_H
